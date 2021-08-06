0c293b1d1a425205b1a8b4322cf37eadd1afa4e4
bazz666 <herrera.se1992@gmail.com>
2021-08-05 23:00:45 -0400

    ecommerce base

---
+++ Dropbox/Mi PC (LAPTOP-4A9P8DN5)/Desktop/ecommerce/app/models/product.rb
@@ -0,0 +1,12 @@
+class Product < ApplicationRecord
+  has_and_belongs_to_many :categories
+
+  has_many :order_items
+  has_many :orders, through: :order_items
+
+  validates :categories, presence: true
+  validates :name, presence: true
+  validates :stock, presence: true
+  validates :price, presence: true
+  validates :sku, presence: true
+end
